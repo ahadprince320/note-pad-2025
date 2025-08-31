f5eb68d493c5ed9fdc40c6012ce9421edc96c108
MD.Prins Ahad <151132033+ahadprince320@users.noreply.github.com>
2025-08-30 21:12:09 -0700

    therad update

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -11,23 +11,26 @@ class home_screen extends StatefulWidget {
 class _home_screenState extends State<home_screen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: ListView.builder(
-          itemCount: 1000,
-          itemBuilder: (context, index) {
-            return ListTile(
-              onTap: () {
-                Navigator.pushNamed(context, '/note_screen',arguments: 'title' + index.toString() );
-              },
-              title: Text('title' + index.toString()),
-              subtitle: Text('Wite some thing about you' + index.toString()),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                  children: [Icon(Icons.edit),Icon(Icons.delete),]),
-            );
-          },
-        ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        title: Text('Note Book',style:TextStyle(color: Colors.white,fontWeight: FontWeight.bold) ,),
+      ),
+      
+      body: ListView.builder(
+        itemCount: 1000,
+        itemBuilder: (context, index) {
+          return ListTile(
+            onTap: () {
+              Navigator.pushNamed(context, '/note_screen',arguments: 'title' + index.toString() );
+            },
+            title: Text('title' + index.toString()),
+            subtitle: Text('Wite some thing about you' + index.toString()),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+                children: [Icon(Icons.edit),Icon(Icons.delete),]),
+          );
+        },
       ),
     );
   }
